db7f582358ee811c896f7203a8cae16236b57f49
Louie D <louie.delrosario97@gmail.com>
2026-06-22 15:49:05 -0700

    Setting up the CategoriesController methods, Phase 1

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,8 +1,11 @@
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.yearup.models.Category;
 import org.yearup.models.Product;
 import org.yearup.service.CategoryService;
@@ -10,12 +13,12 @@ import org.yearup.service.ProductService;
 
 import java.util.List;
 
-// add the annotations to make this a REST controller
+// [ COMPLETE ] add the annotations to make this a REST controller
 @RestController
-// add the annotation to make this controller the endpoint for the following url
+// [ COMPLETE ] add the annotation to make this controller the endpoint for the following url
     // http://localhost:8080/categories
 @RequestMapping("categories")
-// add annotation to allow cross site origin requests
+// [ COMPLETE ] add annotation to allow cross site origin requests
 @CrossOrigin
 
 public class CategoriesController
@@ -24,7 +27,7 @@ public class CategoriesController
     private ProductService productService;
 
 
-    // create an Autowired constructor to inject the categoryService and productService
+    // [ COMPLETE ] create an Autowired constructor to inject the categoryService and productService
     @Autowired
     public CategoriesController(CategoryService categoryService, ProductService productService)
     {
@@ -32,18 +35,29 @@ public class CategoriesController
         this.productService = productService;
     }
 
-    // add the appropriate annotation for a get action
+    // [ COMPLETE ] add the appropriate annotation for a get action
+    @GetMapping("")
+    @PreAuthorize("permitAll()")
     public List<Category> getAll()
     {
-        // find and return all categories
-        return null;
+        // [ COMPLETE ] find and return all categories
+        // [ IN PROGRESS ] business logic for 'getAllCategories'
+        return categoryService.getAllCategories();
     }
 
-    // add the appropriate annotation for a get action
+    // [ COMPLETE ] add the appropriate annotation for a get action
+    @GetMapping("{id}")
+    @PreAuthorize("permitAll()")
     public Category getById(@PathVariable int id)
     {
-        // get the category by id
-        return null;
+        // [ COMPLETE ] get the category by id
+        // [ IN PROGRESS ] business logic for 'getById'
+        Category category = categoryService.getById(id);
+
+        if (category == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        return category;
     }
 
     // the url to return all products in category 1 would look like this
@@ -52,7 +66,8 @@ public class CategoriesController
     public List<Product> getProductsById(@PathVariable int categoryId)
     {
         // get a list of product by categoryId
-        return null;
+
+        return productService;
     }
 
     // add annotation to call this method for a POST action
