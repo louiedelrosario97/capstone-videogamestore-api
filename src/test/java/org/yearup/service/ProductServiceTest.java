6cdce3e5b31f408599f96927c6584274f49d1197
Louie D <louie.delrosario97@gmail.com>
2026-06-23 16:04:47 -0700

    Added a unit test for Bug 1 using the Mockito framework. Search should return featured and non-featured products

--- src/test/java/org/yearup/service/ProductServiceTest.java
+++ src/test/java/org/yearup/service/ProductServiceTest.java
@@ -5,9 +5,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.yearup.models.Product;
 import org.yearup.repository.ProductRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 // workbook 8a pg. 89 - how to write tests for Service Layer
 @ExtendWith(MockitoExtension.class)
 class ProductServiceTest
@@ -22,8 +28,22 @@ class ProductServiceTest
     public void searchNoFiltersApplied_shouldReturnBoth_featuredAndNonFeaturedProducts()
     {
         // arrange
+        Product featured = new Product();
+        featured.setFeatured(true);
+        Product notFeatured = new Product();
+        notFeatured.setFeatured(false);
+
+        List<Product> mockList = new ArrayList<>();
+        mockList.add(featured);
+        mockList.add(notFeatured);
+
+        when(productRepository.findAll()).thenReturn(mockList);
+
         // act
+        List<Product> results = productService.search(null, null, null, null);
+
         // assert
+        assertEquals(2, results.size());
     }
 
 
