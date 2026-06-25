9c221ca8c56f7bafe0231212243cedbd777a7415
Louie D <louie.delrosario97@gmail.com>
2026-06-24 19:37:17 -0700

    created POST business logic for ShoppingCartService + notes

--- src/main/java/org/yearup/service/ShoppingCartService.java
+++ src/main/java/org/yearup/service/ShoppingCartService.java
@@ -23,16 +23,16 @@ public class ShoppingCartService
     }
 //---------------------------------------- [ GET getByUserId() ] --------------------------------------------------
 
+    // instructions: load the user's cart rows, look up each product, and build the ShoppingCart
+
+    // notes:
+    // we want data from our DB, so we create a list that calls the repository. (repo connects to DB)
+    // CartItem's variables are just IDs, so we need to use the 'productId' to fetch the full data
+    // we'll use ProductService's getById() and pass in CartItem's getProductId()
+    // ShoppingCartItem combines the Product data from ProductService with the quantity from the CartItem row.
+
     public ShoppingCart getByUserId(int userId)
     {
-        // instructions: load the user's cart rows, look up each product, and build the ShoppingCart
-
-        // notes:
-        // we want data from our DB, so we create a list that calls the repository. (repo connects to DB)
-        // CartItem's variables are just IDs, so we need to use the 'productId' to fetch the full data
-        // we'll use ProductService's getById() and pass in CartItem's getProductId()
-        // ShoppingCartItem combines the Product data from ProductService with the quantity from the CartItem row.
-
         List<CartItem> usersCart = shoppingCartRepository.findByUserId(userId);
 
         ShoppingCart cart = new ShoppingCart();
@@ -44,21 +44,44 @@ public class ShoppingCartService
             ShoppingCartItem shoppingCartItem = new ShoppingCartItem();
 
             shoppingCartItem.setProduct(product);
-
             shoppingCartItem.setQuantity(cartItem.getQuantity());
 
             cart.add(shoppingCartItem);
         }
         return cart;
     }
+// ----------------------------------- [ POST - addProductToCart() ] ---------------------------------------------------
 
-// ----------------------------------- [ POST - updateCart() ] --------------------------------------------------------
+    // instructions: if the product we are adding is not yet in the cart, we insert the product with a quantity of 1
+    //               if the product already exists in the cart, increase the quantity by 1
 
-    public ShoppingCart updateProduct(int userId, int productId, int quantity)
+    // notes: we need to check the DB to see if the product we are adding already exists in the users cart
+    // for both outcomes, we should create an if-else block.
+    // to see the DB, we should call the ShoppingCartRepo. (the repo has a method called findByUserIdAndProductId(), perfect.)
+    // findByUserIdAndProductId() passes userId and productId (CartItem's data members)
+    //
+    // - [if] cartItem equals 'null' then we add the cartItem's data, set the quantity to 1, and .save to the repo
+    // - [else] increase the cartItem quantity by 1. .setQuantity then .save
+
+    public ShoppingCart addProductToCart(int userId, int productId)
     {
+        CartItem existing = shoppingCartRepository.findByUserIdAndProductId(userId, productId);
 
+        if  (existing == null)
+        {
+            CartItem newItem = new CartItem();
+            newItem.setUserId(userId);
+            newItem.setProductId(productId);
+            newItem.setQuantity(1);
+            shoppingCartRepository.save(newItem);
+        }
+        else
+        {
+            existing.setQuantity(existing.getQuantity() + 1);
+            shoppingCartRepository.save(existing);
+        }
+        return getByUserId(userId); // returns same method signature + re-queries the DB
     }
-
 
 // ----------------------------------- [ DELETE - clearCart() ] -------------------------------------------------------
 
@@ -67,9 +90,9 @@ public class ShoppingCartService
 
     }
 
-// ----------------------------------- [ PUT - addProductToCart() ] ---------------------------------------------------
+// ----------------------------------- [ PUT - updateCart() ] --------------------------------------------------------
 
-    public ShoppingCart addProductToCart(int userId, int productId)
+    public ShoppingCart updateProduct(int userId, int productId, int quantity)
     {
 
     }
