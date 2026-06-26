d3eaf6b42a75258661b076fa00d9b27db34603bb
Louie D <louie.delrosario97@gmail.com>
2026-06-25 20:32:46 -0700

    There was no logic in the repo to get the profile data. Created method that gets a Profile by calling its userId

--- src/main/java/org/yearup/repository/ProfileRepository.java
+++ src/main/java/org/yearup/repository/ProfileRepository.java
@@ -4,7 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.yearup.models.Profile;
 
+import java.util.List;
+
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Integer>
 {
+    Profile findByUserId(int userId);
 }
+
+// notes: create a method that gets all the profiles by calling its primary key
+// (changed return type to Profile instead of List<Profile> since there is only one instance of each profile.)
